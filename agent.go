f2b498f36c8f9577786afc33fee7a16e819a9fbf
Tom C <tom@connectedinteractive.com>
2017-03-22 16:05:36 -0700

    upping bid price to .00

--- agent.go
+++ agent.go
@@ -254,7 +254,7 @@ func (agent *Agent) DoBid(
 
 		bidID := strconv.Itoa(agent.bidID)
 
-		price := randomPrice(0.25, 2.25)
+		price := randomPrice(0.25, 4.00)
 
 		log.Println("The price bid is: ", price)
 
